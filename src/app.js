9193dfea07a3db9ae483276734ed45e0cb0d29a3
naral <naral@pinterest.com>
2022-03-31 19:17:38 -0600

    setup a random api as a template

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const { default: helmet } = require('helmet');
 
 const middlewares = require('./middlewares');
+const api = require('./api');
+
 const app = express();
 
 app.set('json spaces', '\t');
@@ -15,6 +17,9 @@ app.get('/', (req, res) => res.json({ message: 'Halo 👋' }));
 app.use(middlewares.checkApiKey);
 app.use(middlewares.checkRequiredHeaders);
 app.use(middlewares.handleCors);
+
+app.use('/api', api);
+
 app.use(middlewares.handleNotFound);
 app.use(middlewares.handleErrors);
 
